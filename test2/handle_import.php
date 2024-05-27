323d0c7a60915b44e75260370de04ab449a906f6
ST <29339618+SteveDevs@users.noreply.github.com>
2024-05-27 23:17:54 +0200

    Update handle_import.php

--- test2/handle_import.php
+++ test2/handle_import.php
@@ -1,9 +1,9 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DSN', 'mysql:host=localhost;dbname=steve_devprox');
+define('DSN', 'mysql:host=localhost;dbname=test');
 define('DB_USER', 'admin');
-define('DB_PASS', 'Hasher_80');
-define('DB_NAME', 'steve_devprox');
+define('DB_PASS', 'password');
+define('DB_NAME', 'test');
 
 spl_autoload_register(function($class){
     require str_replace("\\", "/", $class).".php";
