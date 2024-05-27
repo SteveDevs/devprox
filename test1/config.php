2ef96f8053e6e2c9d1083dbad2107ccd2003bfb5
ST <29339618+SteveDevs@users.noreply.github.com>
2024-05-27 23:14:03 +0200

    Update config.php

--- test1/config.php
+++ test1/config.php
@@ -3,4 +3,4 @@ define('DB_HOST', 'localhost');
 define('DSN', 'mysql:host=localhost;dbname=test');
 define('DB_USER', 'user');
 define('DB_PASS', 'password');
-define('DB_NAME', 'steve_devprox');
+define('DB_NAME', 'test');
